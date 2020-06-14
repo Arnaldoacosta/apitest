18f00ea96bd57a479d60624f4fe772aa74902db5
ferchufer1 <64679317+ferchufer1@users.noreply.github.com>
2020-06-14 01:00:26 -0300

    agrego db and endpoint

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask import jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 def create_app():
     app = Flask(__name__)
     return app
 
 app = create_app()
+
+db = SQLAlchemy(app)
 
 @app.route('/test', methods=['GET'])
 def get_users():
@@ -22,6 +25,10 @@ def get_metodo2():
     response = {'message': 'probando2'}
     return jsonify(response)
 
+
+@app.route("/test2")
+def imprimirJson():
+    return 'yes base de datos'
 
 
 if __name__ == '__main__':
