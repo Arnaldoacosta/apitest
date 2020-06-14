661b8052be3593017f2f87ad0bd2a66e92da5ca4
ferchufer1 <64679317+ferchufer1@users.noreply.github.com>
2020-06-14 01:46:46 -0300

    3 archivo de pruebas

---
+++ Servicio/Internal_errors.txt
@@ -0,0 +1,6 @@
+
+class CodeInternalError:
+    ERROR_INTERNAL_10_JSON_BAD_FORMED=10
+    ERROR_INTERNAL_11_CONEXION_BD=11 #errores relacionados con la base de datos
+    ERROR_INTERNAL_12_REQUEST_NOT_FOUND=12 #resultados no encontrados
+    ERROR_INTERNAL_13_REQUEST_DATA_NOT_MATCHED=13 # datos recibidos no son validos
--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, request, Response
 from flask import jsonify
+from flask_sqlalchemy import SQLAlchemy
+from Servicio import servicio
+from config import Production
+import requests
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Production)
     return app
 
+
 app = create_app()
+
+db = SQLAlchemy(app)
 
 @app.route('/test', methods=['GET'])
 def get_users():
@@ -16,6 +24,11 @@ def get_users():
 def get_metodo():
     response = {'message': 'probando2'}
     return jsonify(response)
+
+@app.route('/materias/<int:id>', methods=['GET'])
+def findNotasMateriasByAlumnoID(id):      
+        return (servicio.findNotasMateriasByAlumnoID(str(id)))
+
 
 
 if __name__ == '__main__':
