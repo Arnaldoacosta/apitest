b1f25f2559fcde07a2f204a0b8987ef3429ba26c
ferchufer1 <64679317+ferchufer1@users.noreply.github.com>
2020-06-30 20:12:13 -0300

    validacion de materia repetida

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ class Production(object):
     """
     Production environment configurations
     """
-    #SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-    SQLALCHEMY_DATABASE_URI="postgresql://postgres:Ingreso871@localhost/Irso"
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    #SQLALCHEMY_DATABASE_URI="postgresql://postgres:Ingreso871@localhost/Irso"
     SQLALCHEMY_TRACK_MODIFICATIONS= False
     #JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
 
