9d44ef9ed3059594e8783e1d416906b6975a7b0b
ferchufer1 <64679317+ferchufer1@users.noreply.github.com>
2020-07-07 00:57:32 -0300

    se actualiza config

--- config.py
+++ config.py
@@ -5,9 +5,8 @@ class Production(object):
     """
     Production environment configurations
     """
-    #SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS= False
-    SQLALCHEMY_DATABASE_URI="postgresql://postgres:Ingreso871@localhost/Irso"
     #JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
 
 app_config = {
